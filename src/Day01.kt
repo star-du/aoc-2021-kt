8dcdb03728216088e8f5b7fa90b9992cf90d8fbf
star-du <star.duxy@outlook.com>
2023-04-18 20:54:49 +0800

    Day 1 completes.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val measurements = input.mapNotNull { s -> s.toIntOrNull() }
+        if (measurements.size <= 1) {
+            return 0
+        }
+
+        var count = 0
+        for (i in 0 until measurements.size - 1) {
+            if (measurements[i + 1] > measurements[i]) {
+                count += 1
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowSize = 3
+        val measurements = input.mapNotNull { s -> s.toIntOrNull() }
+        if (measurements.size <= windowSize) {
+            return 0
+        }
+
+        var count = 0
+        for (i in 0 until measurements.size - windowSize) {
+            if (measurements[i + 3] > measurements[i]) {
+                count += 1
+            }
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     part1(input).println()
