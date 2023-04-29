5912cb9a96c55d03938f7779d818bbe47aeeb5a2
star-du <star.duxy@outlook.com>
2023-04-29 19:24:14 +0800

    Day 3 update.
    Add alternative solution.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,59 +1,18 @@
 fun main() {
+    fun mostCommonChar(l: List<Char>): Char {
+        var ones = 0
+        l.forEach { if (it == '1') ones += 1 else ones -= 1 }
+        return if (ones >= 0) '1' else '0'
+    }
+
     fun mostCommonBinaryString(input: List<String>, width: Int): String {
         val mostCommon = StringBuilder(width)
         for (i in 0 until width) {
-            var ones = 0
-            for (row in input) {
-                if (row[i] == '1') {
-                    ones += 1
-                } else {
-                    ones -= 1
-                }
-            }
-            val mostCommonCharInColumn: Char =
-                if (ones >= 0) '1' else '0'
+            val mostCommonCharInColumn = mostCommonChar(input.map { it[i] })
             mostCommon.append(mostCommonCharInColumn)
         }
 
         return mostCommon.toString()
-    }
-
-    fun leastCommonBinaryString(input: List<String>, width: Int): String {
-        val leastCommon = StringBuilder(width)
-        for (i in 0 until width) {
-            var zeros = 0
-            for (row in input) {
-                if (row[i] == '1') {
-                    zeros += 1
-                } else {
-                    zeros -= 1
-                }
-            }
-            val leastCommonCharInColumn: Char =
-                if (zeros >= 0) '0' else '1'
-            leastCommon.append(leastCommonCharInColumn)
-        }
-
-        return leastCommon.toString()
-    }
-
-    fun findClosest(candidate: List<String>, target: String): String {
-        val candidateSimilarityList = candidate.associateBy { s ->
-            var score = 0
-            for (i in target.indices) {
-                if (target[i] == s[i]) {
-                    score += 1
-                } else {
-                    break
-                }
-            }
-            score
-        }
-
-        return candidateSimilarityList
-            .maxWithOrNull(compareBy { it.key })
-            ?.value
-            ?: ""
     }
 
     fun part1(input: List<String>): Int {
@@ -78,19 +37,21 @@ fun main() {
     fun part2(input: List<String>): Int {
         if (input.isEmpty()) return 0
 
-        val myComparator = compareBy<Map.Entry<Char, Int>> { it.value }.thenBy { it.key == '1' }
+        // Scheme 1
+        val candidate = input.toMutableList()
+        val indices = input[0].indices
 
-        var mostCommonWinner = input
-        for (i in input[0].indices) {
-            val charWithCount: Map<Char, Int> =
-                mostCommonWinner.map { it[i] }.groupingBy { it }.eachCount()
-            val mostCommonChar = charWithCount.maxWith(myComparator).key
-            mostCommonWinner = mostCommonWinner.filter { it[i] == mostCommonChar }
-            if (mostCommonWinner.size == 1) break
+        for (i in indices) {
+            val char = mostCommonChar(candidate.map { s -> s[i] })
+            candidate.retainAll { it[i] == char }
+            if (candidate.size == 1) break
         }
-        println(mostCommonWinner[0])
-        val oxygenGeneratorRating = Integer.parseInt(mostCommonWinner[0], 2)
 
+        assert(candidate.size == 1)
+        val oxygenGeneratorRating = Integer.parseInt(candidate[0], 2)
+
+        // Scheme 2
+        val myComparator = compareBy<Map.Entry<Char, Int>> { it.value }.thenBy { it.key == '1' }
         var leastCommonWinner = input
         for (i in input[0].indices) {
             val charWithCount: Map<Char, Int> =
@@ -99,7 +60,6 @@ fun main() {
             leastCommonWinner = leastCommonWinner.filter { it[i] == leastCommonChar }
             if (leastCommonWinner.size == 1) break
         }
-        println(leastCommonWinner[0])
         val co2ScrubberRating = Integer.parseInt(leastCommonWinner[0], 2)
 
         if (debug) println("oxygenGeneratorRating = $oxygenGeneratorRating")
